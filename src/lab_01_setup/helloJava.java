13f4dd158460e353284e2f8f42782d920a1fe313
ntpdlp <ntpdlp@gmail.com>
2025-06-09 14:48:47 -0700

    lab01_Byte.MIN-VALUE

--- src/lab_01_setup/helloJava.java
+++ src/lab_01_setup/helloJava.java
@@ -4,6 +4,7 @@ package lab_01_setup;
 public class helloJava{
     public static void main(String[] args){
         System.out.println("hello Java");
-        System.out.println();
+        System.out.println(Byte.MAX_VALUE); // 0 -> 127
+        System.out.println(Byte.MIN_VALUE); // -1 -> -128
     }
 }
