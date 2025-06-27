88826bc575e321d5fd750e5b67b4c7957917f040
ntpdlp <ntpdlp@gmail.com>
2025-06-27 11:15:24 -0700

    lab 04

---
+++ src/lab_04_method/lab_04_NumberGame.java
@@ -0,0 +1,25 @@
+package lab_04_method;
+
+import java.util.Scanner;
+
+public class lab_04_NumberGame {
+
+    public static void main(String[] args) {
+
+        int userOption = printMenu();
+    }
+
+    private static int printMenu() {
+
+        System.out.println("=============MENU===========");
+        System.out.println("1. Print all numbers");
+        System.out.println("2. Print maximum value");
+        System.out.println("3. Print minimum value");
+        System.out.println("4. Search number");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+        return 0;
+    }
+
+}
