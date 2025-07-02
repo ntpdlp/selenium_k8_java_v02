fd72988ac7ec43829123ffc565af842b7df66633
ntpdlp <ntpdlp@gmail.com>
2025-07-02 14:51:10 -0700

    lab 04

--- src/lab_04_method/lab_04_NumberGame.java
+++ src/lab_04_method/lab_04_NumberGame.java
@@ -1,12 +1,46 @@
 package lab_04_method;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class lab_04_NumberGame {
 
     public static void main(String[] args) {
 
-        int userOption = printMenu();
+        String isPlaying = "yes";
+        ArrayList<Integer> generatedListNumbers = generateAllNumbers();
+        while(isPlaying.equals("yes")){
+            int userOption = printMenu();
+            switch (userOption){
+                case 1:
+                    System.out.println(generatedListNumbers);
+                    break;
+                case 2:
+                    findMax(generatedListNumbers);
+                    break;
+                case 3:
+                    findMin(generatedListNumbers);
+                    break;
+                case 4:
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.println("Enter the search number: ");
+                    Integer number = scanner.nextInt();
+                    String findingResult = (searchNumber(generatedListNumbers,number) == true)?"found":"not found";
+                    System.out.println(findingResult);
+                    break;
+                default:
+                    System.out.println("Wrong choice! please read menu carefully");
+            }
+            System.out.println("Would you want to continue? yes/no: ");
+            Scanner scanner = new Scanner(System.in);
+            isPlaying = scanner.next().toLowerCase();
+            if (isPlaying.equals("no")){
+                System.out.println("Thank you!");
+            }
+        }
+
     }
 
     private static int printMenu() {
@@ -19,8 +53,50 @@ public class lab_04_NumberGame {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your choice: ");
-        int choice = scanner.nextInt();
+        int choice =  scanner.nextInt();
         return choice;
+
+    }
+
+    private static ArrayList<Integer> generateAllNumbers(){
+        ArrayList<Integer> nums = new ArrayList<>();
+        for (int index = 0; index < 10; index++) {
+            Integer num = new SecureRandom().nextInt(1000);
+            nums.add(num);
+        }
+        return nums;
+    }
+
+
+    private static void findMax(ArrayList<Integer> list){
+        Integer max = list.get(0);
+        for (Integer item : list) {
+            if(max < item){
+                max = item;
+            }
+        }
+        System.out.println("Maximum number is: " + max);
+    }
+
+
+    private static void findMin(ArrayList<Integer> list){
+        Integer min = list.get(0);
+        for (Integer item : list) {
+            if(min > item){
+                min = item;
+            }
+        }
+        System.out.println("Minimum number is: " + min);
+    }
+    
+    private static boolean searchNumber(ArrayList<Integer> list, Integer number){
+
+        for (Integer item : list) {
+            if(item.equals(number)){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
