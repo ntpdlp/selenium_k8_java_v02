fb84b24afb5125899986b31589d7f21a77edd8b2
ntpdlp <30646772+ntpdlp@users.noreply.github.com>
2025-09-22 15:10:46 -0700

    OOP practice

---
+++ src/lab_06_OOPConcept/DrumKitApp/DrumKit.java
@@ -0,0 +1,18 @@
+package lab_06_OOPConcept.DrumKitApp;
+
+public class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare(){
+        System.out.println("bang bang be-bang");
+    }
+
+    void playTopHat(){
+        System.out.println("ding ding da-ding");
+    }
+}
+
+
+
+
---
+++ src/lab_06_OOPConcept/DrumKitApp/DrumKitTestDrive.java
@@ -0,0 +1,14 @@
+package lab_06_OOPConcept.DrumKitApp;
+
+public class DrumKitTestDrive {
+
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+        d.snare = false;
+        if(d.snare == true){
+            d.playSnare();
+        }
+        d.playSnare();
+        d.playTopHat();
+    }
+}
