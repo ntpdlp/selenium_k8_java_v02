fd6fefd0f82037e45155a5b4881c26171c0bb9b2
ntpdlp <30646772+ntpdlp@users.noreply.github.com>
2025-12-17 21:30:40 -0800

    lab 16: serialVersionUID add

--- src/lab_16_serialization_fileIO/serializationLearning/Student.java
+++ src/lab_16_serialization_fileIO/serializationLearning/Student.java
@@ -1,8 +1,11 @@
 package lab_16_serialization_fileIO.serializationLearning;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class Student implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -6157283002164137071L;
     public String name;
     public int age;
 
