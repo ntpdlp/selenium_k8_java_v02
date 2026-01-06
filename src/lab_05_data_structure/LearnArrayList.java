456cd1e1525d90405ed398f06b1a436634628a5b
ntpdlp <ntpdlp@gmail.com>
2026-01-05 21:54:48 -0800

    update ArrayList with 3 ways iterating

--- src/lab_05_data_structure/LearnArrayList.java
+++ src/lab_05_data_structure/LearnArrayList.java
@@ -1,61 +1,57 @@
 package lab_05_data_structure;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class LearnArrayList {
 
     public static void main(String[] args) {
-
         ArrayList<Integer> myNums = new ArrayList<>();
 
         //common methods: add(),get(),set(),remove(),size()
-        //use index or iterator to loop through elements
         myNums.add(1);
         myNums.add(5);
         myNums.add(8);
         myNums.add(9);
         System.out.println("size at initial: " + myNums.size()); //4
         System.out.println(myNums);
-
         Integer myChoice = myNums.get(1); //get(index) >> 5
-        myNums.set(1,50); //set(index,value)
+        myNums.set(1, 50); //set(index,value)
         System.out.println("update value at 1 position, old value: " + myChoice); //5
         System.out.println(myNums);
-
         myNums.remove(0); // remove(index)
         System.out.println("size after removing first element: " + myNums.size()); //3
         System.out.println(myNums);
 
-
-        //loop through elements by using index loop
-        int myNumsSize = myNums.size();
-        for (int index = 0; index < myNumsSize; index++) {
+        //3 ways to loop through an arrayList elements
+        // 01: by using loop with for
+        // 02: by using loop with for enhanced
+        // 03: iterator
+        for (int index = 0; index < myNums.size(); index++) {
             System.out.print(myNums.get(index) + "\t");
         }
         System.out.println("\r\n------------------");
 
-        //loop through elements by using Iterator
+        for (Integer num : myNums) {
+            System.out.print(num + "\t");
+        }
+        System.out.println("\r\n------------------");
+
         Iterator<Integer> iterMyNums = myNums.iterator();
-        while(iterMyNums.hasNext()){
+        while (iterMyNums.hasNext()) {
             System.out.print(iterMyNums.next() + "\t");
         }
         System.out.println("\r\n------------------");
 
-
         //sort
-        List<String> names = new ArrayList<>();
-        names.add("Jamie");
-        names.add("Peggy");
-        names.add("Tom");
-        names.add("Adnan");
-        names.add("Vovo");
-        System.out.println("Before: " + names);
+        List<String> inputNames = List.of("Jamie", "Peggy", "Tom", "Vovo", "Mike");
+        List<String> names = new ArrayList<>(inputNames);
+        System.out.println("Original list: " + names);
         Collections.sort(names);
         System.out.println("After sorting: " + names);
-
-        Collections.sort(names,Collections.reverseOrder());
+        Collections.sort(names, Collections.reverseOrder());
         System.out.println("Reverse Order sorting: " + names);
-
-
     }
 }
